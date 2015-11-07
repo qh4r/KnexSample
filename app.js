7372b87064688341d40e1340f2cc19f8578fde4f
Rafał Kucharski <r.kucharski@lgbs.pl>
2015-11-08 00:02:49 +0100

    Dodano treeize + przykład użycia

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var cfg = require('./knex-config');
-knex = require('knex')(cfg);
+var cfg = require('./knex-config'),
+    knex = require('knex')(cfg),
+    Treeize = require('treeize');
 
 //czysci terminal (funkcja clear)
 process.stdout.write("\033c");
 
 //wypisuje treść kwerendy sqlowej toSQL() daje więcej info
-var query = knex.select('title', 'rating').from('book');
+var query = knex.select('title', 'rating').from('book').where({
+    author_id: 1
+});
 console.log(query.toString());
 
 //wykonuje zapytanie
@@ -51,5 +54,25 @@ knex("book")
         knex.destroy();
         console.log('\nfinally');
     });
+
+knex("book")
+    .select(["book.author_id as book:author_id", "book.title as book:title", "book.rating as book:rating", "author.firstname as author:firstname", 'author.lastname as author:lastname'])
+    .join("author", "author.id", "=", "book.author_id")
+    .then(function(result){
+        var tree = new Treeize();
+        tree.grow(result);
+        return tree.getData();
+    })
+    .then(function (result) {
+        console.log("\n tree \n");
+        console.log(result);
+    })
+    .catch(function (err) {
+        console.log("\n tree \n");
+        console.log(err);
+    }).finally(function () {
+        knex.destroy();
+    });
+
 
 console.log("Done. ");
